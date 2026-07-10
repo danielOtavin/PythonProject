436152bce2f97ef64cb744ae4ebf00808012fbf8
DanielOtavin <you@example.com>
2026-07-10 20:17:30 +0300

    добавил метод нажатия кнопок для страницы sql

--- pages/sql_page.py
+++ pages/sql_page.py
@@ -12,8 +12,12 @@ class SQLPage(BasePage):
     COMMAND_RADIO_BUTTON = '//button[@id="command-radio"]'
     SEND_REQUEST_BUTTON = '//button[@id="send-request"]'
 
-
-    def send_sql_query(self, text: str):
+    def send_sql_query_or_command(self,  text: str, button: Literal['query', 'command']) -> None:
+        selected_button = {'query': self.QUERY_RADIO_BUTTON,
+                           'command': self.COMMAND_RADIO_BUTTON}
+        if button not in selected_button:
+            raise ValueError(f'Неизвестная кнопка: {button}')
+        btn = selected_button[button]
         self.input_value(self.TEXT_AREA, text)
         self.click_element(self.QUERY_RADIO_BUTTON)
         self.click_element(self.SEND_REQUEST_BUTTON)
