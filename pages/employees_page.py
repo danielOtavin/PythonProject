16a45af7a1c1458c664e712a4ed885e612b59e23
DanielOtavin <you@example.com>
2026-07-10 20:14:27 +0300

    обновил метод нажатия кнопок на странице сотрудников

--- pages/employees_page.py
+++ pages/employees_page.py
@@ -16,12 +16,13 @@ class EmployeePage(BasePage):
     def get_employee_item(self, emp_id: int = 2) -> str:
         return f'//div[@data-employee-id="{str(emp_id)}"]'
 
-    def click_view_button(self, emp_id: int) -> None:
-        employee = self.find_element(self.get_employee_item(emp_id))
-        button = employee.find_element(By.XPATH, self.VIEW_BUTTON)
-        self.click_element(button)
-
-    def click_update_info_button(self, emp_id: int) -> None:
+    def click_button_employees(self, button: Literal['view','update','delete'], emp_id: int = 2) -> None:
+        button_name = {'view': self.VIEW_BUTTON,
+                       'update': self.UPDATE_INFO_BUTTON,
+                       'delete': self.DELETE_BUTTON}
+        if button not in button_name:
+            raise ValueError(f'Неизвестная кнопка: {button}')
+        selected_button = button_name[button]
         employee = self.find_element(self.get_employee_item(emp_id))
         button = employee.find_element(By.XPATH, self.UPDATE_INFO_BUTTON)
         self.click_element(button)
