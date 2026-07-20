2a054208a79f02b976063c01d11e762e2768896d
DanielOtavin <you@example.com>
2026-07-21 00:34:08 +0300

    дописал методы для работы со страницей сотрудников

--- pages/employees_page.py
+++ pages/employees_page.py
@@ -28,16 +28,53 @@ class EmployeePage(BasePage):
     def get_employee_item(self, emp_id: int = 2) -> str:
         return f'//div[@data-employee-id="{str(emp_id)}"]'
 
-    def click_button_employees(self, button: Literal['view','update','delete'], emp_id: int = 2) -> None:
-        button_name = {'view': self.VIEW_BUTTON,
-                       'update': self.UPDATE_INFO_BUTTON,
-                       'delete': self.DELETE_BUTTON}
-        if button not in button_name:
-            raise ValueError(f'Неизвестная кнопка: {button}')
-        selected_button = button_name[button]
+
+    def set_checkbox(self, checkbox_locator: str, state: bool):
+        checkbox = self.find_element(checkbox_locator)
+        if state and not checkbox.is_selected():
+            checkbox.click()
+        elif not state and checkbox.is_selected():
+            checkbox.click()
+
+
+    def open_content_window(self, emp_id: int, button: Literal['view', 'update', 'delete']):
+        options_dct = {'view': (self.VIEW_BUTTON, self.VIEW_MODAL),
+                       'update': (self.UPDATE_INFO_BUTTON, self.UPDATE_MODAL),
+                       'delete': (self.DELETE_BUTTON, self.DELETE_MODAL)}
+        pick_button, modal = options_dct[button]
         employee = self.find_element(self.get_employee_item(emp_id))
         btn = employee.find_element(By.XPATH, selected_button)
         self.click_element(btn)
+        self.wait_until_visible(modal)
+
+
+    def close_modal_with_esc(self):
+        self.driver.find_element(By.XPATH, "//body").send_keys(Keys.ESCAPE)
+
+
+    def view_employee_data(self, emp_id: int):
+        self.open_content_window(emp_id, 'view')
+        return self.find_element('//div[@id="viewDetails"]').text
+
+
+    def update_employee_data(self, emp_id: int, employee_data: Employee) -> None:
+        self.open_content_window(emp_id, 'update')
+        self.input_value(self.NAME_FIELD, str(employee_data.name))
+        self.input_value(self.SALARY_FIELD, str(employee_data.salary))
+        self.set_checkbox(self.WORK_STATUS_CHECKBOX, employee_data.work)
+        self.click_element(self.SUBMIT_BUTTON)
+        self.wait_until_invisible(self.CONTENT_WINDOW)
+
+    def delete_employee_data(self, emp_id: int) -> None:
+        self.open_content_window(emp_id, 'delete')
+        self.click_element(self.CONFIRM_DELETE_BUTTON)
+        self.wait_until_invisible(self.CONTENT_WINDOW)
+
+
+
+
+
+
 
 
 
